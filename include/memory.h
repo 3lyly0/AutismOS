98a9b493b4a4d8cd3a6c57a6f15a58817021e02b
3lyly0 <3lyly00@gmail.com>
2026-03-21 11:19:59 +0200

    refactor(memory): remove kmalloc/kfree implementation and update memory functions

--- include/memory.h
+++ include/memory.h
@@ -15,13 +15,17 @@ void free_page(void *ptr);
 void paging_init();
 void paging_enable();
 
-void *kmalloc(size_t size);
-void kfree(void *ptr);
+// kmalloc/kfree are now provided by kheap.h/kheap.c
+// Include kheap.h if you need heap allocation functions
 
 // Page directory management for processes
 page_directory_t* create_page_directory(void);
 void switch_page_directory(page_directory_t* dir);
 page_directory_t* get_kernel_page_directory(void);
 void map_page_in_directory(page_directory_t* dir, uint32_t virt_addr, uint32_t phys_addr, uint32_t flags);
+
+// Memory info functions
+uint32_t memory_get_end(void);
+int memory_is_initialized(void);
 
 #endif
